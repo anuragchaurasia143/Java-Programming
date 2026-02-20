9ad47e73046322f6c07c416f11f5f6406758c6c3
Anurag Chaurasia <anuragchaurasia1020@gmail.com>
2026-02-20 21:11:02 +0530

    adding reverse an array, pairs in an array and findind subarray

--- searching.java
+++ searching.java
@@ -1,6 +1,7 @@
 //import java.util.*;
 
 public class searching {
+    // linear search
     static int linearSearch(int number[], int key) {
         
         
@@ -12,6 +13,7 @@ public class searching {
  
         return -1;
     }
+    // largest and smallest in an array
     public static int getlargest(int num[]){
         int largest = Integer.MIN_VALUE;  // -infinity
         int smallest = Integer.MAX_VALUE; // +infinity
@@ -26,6 +28,7 @@ public class searching {
         System.out.println("smallest value is: " + smallest);
         return largest;
     }
+    // binary search
     public static int binarysearch(int marks[], int need){
         int start = 0, end = marks.length-1;
         while (start<=end){
@@ -40,7 +43,47 @@ public class searching {
             }
         }
         return -1;
-    }   
+    }  
+    // reverse an array
+    public static void reverse(int number[]){
+        int first = 0, last = number.length-1;
+        while (first<last){
+            // swap
+            int temp = number[last];
+            number[last] = number[first];
+            number[first] = temp;
+
+            first++;
+            last--;
+        }
+    } 
+    //pairs in an array
+    public static void pairs(int number[]){
+        int totalPairs = 0;
+        for (int i=0; i<number.length; i++){
+            for (int j=i+1; j<number.length; j++){
+                System.out.print("(" + number[i]+"," + number[j]+ ")");
+                totalPairs++;
+            }
+           
+            System.out.println();
+        }
+        System.out.println("total pairs = "  + totalPairs);
+    }
+    // printing all subarrays
+    public static void subarrays(int number[]){
+        for (int i=0; i<number.length; i++){
+            int start = i;
+            for (int j=i; j<number.length; j++){
+                int end = j;
+                for (int k=start; k<=end; k++){
+                    System.out.print(number[k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int number[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
         int key = 3;
@@ -58,6 +101,15 @@ public class searching {
          int need = 16;
 
          System.out.println("key found at index: " + binarysearch(number, key));
+         
+
+        reverse (number);
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i] + " ");
+        }
+         pairs(number);
+        subarrays(number);  
     }
+   
 }
 
