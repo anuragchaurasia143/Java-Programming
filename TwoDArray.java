1d76f91477a2cb68e9a90453ab4bef36213ec3b6
Anurag Chaurasia <anuragchaurasia1020@gmail.com>
2026-06-14 08:19:36 +0530

    2D Array Concept

---
+++ TwoDArray.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][3];
+        Scanner sc = new Scanner(System.in);
+
+        //input
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[i].length; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //output
+        for (int i=0; i<arr.length; i++){
+            for (int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
